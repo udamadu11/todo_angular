91c58527c517faa93e860bf0218e8ae0f3245429
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-04-07 03:33:19 +0100

    addComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+//component
+import { TodoComponent } from './todo/todo.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
+const routes: Routes = [
+  {
+    path: '',
+    component: TodoComponent,
+    data: {title: 'List of todos'}
+  },
+  {
+    path: 'todo/add',
+    component: TodoAddComponent,
+    data: {title: 'Add Todo'}
+  },
+  {
+    path: 'todo/edit/:id',
+    component: TodoEditComponent,
+    data: {title: 'Edit Todo'}
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
+import { TodoEditComponent } from './todo-edit/todo-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    TodoAddComponent,
+    TodoEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
