7842303bdc87335ed4ddaeb92a6cd778ba03d1b3
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2020-04-11 16:15:59 +0100

    add to do

--- src/app/todo-add/todo-add.component.ts
+++ src/app/todo-add/todo-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import { ApiService } from '../api.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-todo-add',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-add.component.css']
 })
 export class TodoAddComponent implements OnInit {
+  todoForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private router: Router, private api: ApiService) { }
 
-  constructor() { }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    this.todoForm = this.formBuilder.group({
+      title: ['', Validators.compose([Validators.required])],
+    });
   }
 
+  addTodo() {
+    const payload = {
+      title: this.todoForm.controls.title.value,
+    };
+
+    this.api.addTodo(payload)
+      .subscribe(res => {
+          let id = res['_id'];
+          this.router.navigate(['/']);
+        }, (err) => {
+          console.log(err);
+        });
+  }
 }
